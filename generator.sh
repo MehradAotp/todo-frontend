2cc1d65d26a2611a5178291544b789a58f91524b
mohsen <mohsen.sareminia@gmail.com>
2025-03-01 16:19:19 +0330

    Add OpenAPI generator script and configuration

---
+++ generator.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+set -e
+
+BASE_URL="${BASE_URL:-http://127.0.0.1:3001}"
+API_NAME=$1
+TARGET="${2:-api/$API_NAME-json}"
+
+echo "FETCHING $BASE_URL/$TARGET"
+
+openapi-generator-cli generate -i $BASE_URL/$TARGET --generator-name typescript-axios -o app/src/api/$API_NAME --additional-properties=useSingleRequestParameter=true --skip-validate-spec
